bc9424596f2214e226dbea47d0bb8170f15fd3b7
David Day <dj6082013@gmail.com>
2022-07-27 19:16:50 +0800

    Add deployment script.

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+echo "Putting the website into maintenance mode..."
+php artiasn down
+
+echo "Pulling the latest code..."
+git pull
+
+echo "Running composer..."
+rm -rf ./vendor
+composer install --optimize-autoloader --no-dev
+
+echo "Linking storage..."
+php artisan storage:link
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Caching views..."
+php artisan view:cache
+
+echo "Running migrations..."
+php artisan migrate --force
+
+echo "Putting the website back online..."
+php artiasn up
