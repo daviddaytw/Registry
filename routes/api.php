f58100b3c2723f78a6e1b1e5cffd52f0f3b0be15
David Day <dj6082013@gmail.com>
2023-06-15 16:41:25 +0800

    Shorten api route path.

--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::name('api.')->group(function () {
     Route::apiResource('registry', APIController::class);
+
+    Route::controller(APIController::class)->group(function () {
+        Route::get('/{registry}', 'show')->name('registry.show');
+        Route::put('/{registry}', 'update')->name('registry.update');
+    });
 });
